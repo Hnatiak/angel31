1f336d655c4847a83e93c9d1972bd77ee29c507d
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-05-16 15:34:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -470,10 +470,14 @@ def handle_shower_command(message):
         time.sleep(1800)  # Почекати 30 хвилин (1800 секунд)
         bot.send_message(chat_id, 'Фух, все я прийняла душ, отже що тепер робитимемо?')
         is_shower_time = False
-    elif current_time >= time(19, 0) and current_time <= time(19, 45):
-        bot.reply_to(message, 'Гей, перестань!')
-    else:
-        bot.reply_to(message, 'Сталася помилка')
+    try:
+        bot.restrict_chat_member(message.chat.id, message.from_user.id, until_date=int((datetime.now() + timedelta(minutes=1)).timestamp()))
+        user_mention = f"@{message.from_user.username}" if message.from_user.username else message.from_user.first_name
+        bot.send_message(message.chat.id, f"мут 1 хвилину {user_mention}", reply_to_message_id=message.message_id)
+        bot.reply_to(message, "Не гарно підглядати за дівчиною в душі! Тепер подумай як воно!")
+    except Exception as e:
+        print(e)
+        bot.send_message(message.chat.id, "Гей, перестань, мені не приємно!")
 
 
 
