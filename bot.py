b42bbbccb8befafacc7316359a6b8bc71167d6ad
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-05-16 15:18:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,14 +7,14 @@ import random
 import logging
 #import datetime
 #from datetime import datetime
-import time as t
+import time
 #from telebot import types
 #import sqlite3
 #from sqlite3 import Error
 from telebot import TeleBot, types
 #from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 #from telegram.ext import CallbackContext
-from datetime import datetime, timedelta, time
+from datetime import datetime, timedelta
 #import json
 import re
 #import yagmail
@@ -464,17 +464,17 @@ def handle_shower_command(message):
 
     current_time = datetime.utcnow().time()
 
-    if current_time < t.time(13, 0) or current_time > t.time(19, 30):
+    if current_time < time(19, 0) or current_time > time(19, 30):
         bot.reply_to(message, 'Ні, я ще не йду купатися, тому розслабся.')
         return
 
-    if current_time >= t.time(13, 0, 0) and current_time <= t.time(19, 30, 0):
+    if current_time >= time(19, 0) and current_time <= time(19, 30):
         is_shower_time = True
         bot.reply_to(message, 'Я відійшла в душ')
-        t.sleep(1800)  # Почекати 30 хвилин (1800 секунд)
+        time.sleep(1800)  # Почекати 30 хвилин (1800 секунд)
         bot.send_message(chat_id, 'Фух, все я прийняла душ, отже що тепер робитимемо?')
         is_shower_time = False
-    elif current_time >= t.time(13, 0) and current_time <= t.time(19, 30):
+    elif current_time >= time(19, 0) and current_time <= time(19, 45):
         bot.reply_to(message, 'Гей, перестань!')
     else:
         bot.reply_to(message, 'Сталася помилка')
