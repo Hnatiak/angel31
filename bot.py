b90334aabdd77b219773a1077410548900fc430f
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-05-16 19:43:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -460,13 +460,13 @@ def handle_shower_command(message):
 
     current_time = datetime.utcnow().time()
 
-    if current_time >= time(19, 0) and current_time <= time(20, 0):
+    if current_time >= datetime.time(19, 0) and current_time <= datetime.time(20, 0):
         is_shower_time = True
         bot.reply_to(message, 'Я відійшла в душ')
         time.sleep(1800)  # Почекати 30 хвилин (1800 секунд)
         bot.send_message(message.chat.id, 'Фух, все, я прийняла душ. Отже, що тепер робитимемо?')
         is_shower_time = False
-    elif current_time < time(19, 0) or current_time > time(20, 0):
+    elif current_time < datetime.time(19, 0) or current_time > datetime.time(20, 0):
         try:
             bot.restrict_chat_member(
                 message.chat.id, message.from_user.id,
@@ -484,7 +484,7 @@ def handle_shower_command(message):
             print(e)
             bot.send_message(message.chat.id, "Гей, перестань, мені не приємно!")
     else:
-        bot.reply_to(message, 'Ця команда доступна лише з 19:00 до 19:30')
+        bot.reply_to(message, 'Ця команда доступна лише з 19:00 до 20:00')
 
 # @bot.message_handler(commands=['вдуш'])
 # def handle_shower_command(message):
