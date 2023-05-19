f902fa3bdb8dd72524677240decefdbfab678944
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-05-19 22:58:29 +0200

    Update communication.py

--- speakwithbot/communication.py
+++ speakwithbot/communication.py
@@ -8,15 +8,20 @@ bot = telebot.TeleBot(config.TOKEN)
 angel = ['ангелятко', 'ангел', 'ангелику', 'ангелочок']
 
 def handle_commands(bot, message):
+#     text = message.text.lower()
+
+#     hello_patterns = [r"ангел[и]?к?", r"хелоу", r"привіт", r"здоров", r"салют"]
+#     how_are_you_patterns = [r"як справи", r"ти як", r"як ти"]
+#     dorechi_pattern = r"доречі"
+
+#     hello_found = any(re.search(pattern, text) for pattern in hello_patterns)
+#     how_are_you_found = any(re.search(pattern, text) for pattern in how_are_you_patterns)
+#     dorechi_found = re.search(dorechi_pattern, text)
+
     text = message.text.lower()
 
-    hello_patterns = [r"ангел[и]?к?", r"хелоу", r"привіт", r"здоров", r"салют"]
-    how_are_you_patterns = [r"як справи", r"ти як", r"як ти"]
-    dorechi_pattern = r"доречі"
-
-    hello_found = any(re.search(pattern, text) for pattern in hello_patterns)
-    how_are_you_found = any(re.search(pattern, text) for pattern in how_are_you_patterns)
-    dorechi_found = re.search(dorechi_pattern, text)
+    hello = {"ангел привіт", "ангел здоров", "ангел хай"}
+    how_are_you = {"ангел як справи", "ангел ти як"}
 
     if any(command in text for command in hello):
         bot.send_message(message.chat.id, "Привіт хлопче!")
