4cec0b4deeccbc7e540cbb0bf9c7db9c07ca46aa
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-03 18:11:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,54 +58,6 @@ def send_email_message(message):
 pending_friendships = {}
 friendships = []
 
-def translate_russian_to_ukrainian(word):
-    translation_dict = {
-        'спасиба': 'Дякую',
-        'што': 'що',
-        'шо': 'що',
-        'что': 'що',
-        'чо': 'що',
-        'когда': 'коли',
-        'как': 'як',
-        'где': 'де',
-        'но': 'але',
-        'чиво': 'чого',
-        'чево': 'чого',
-        'чего': 'чого',
-        'да': 'так',
-        'нет': 'ні',
-        'не': 'ні',
-        'канеш': 'звісно',
-        'канешно': 'звісно',
-        'только': 'тільки/лише',
-        'ладно': 'гаразд/окей',
-        'ребят': 'друзі',
-        'даровка': 'здоров',
-        'даров': 'здоров',
-        'хотел': 'здоров',
-        'иди': 'йди/іди',
-        'мать': 'мати',
-        'почему': 'чому',
-        'почти': 'майже',
-        'тебе': 'тобі',
-        'мне': 'мені',
-        'ему': 'йому',
-        # Додайте сюди інші слова та їх переклади
-    }
-    return translation_dict.get(word, word)
-
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    text = message.text.lower()
-    words = re.findall(r'\b\w+\b', text)  # Знаходимо окремі слова в тексті
-
-    for word in words:
-        ukrainian_word = translate_russian_to_ukrainian(word)
-        if word != ukrainian_word:
-            reply = f"{word} немає в українській мові, правильно {ukrainian_word}"
-            bot.reply_to(message, reply)
-            break
-
 
 @bot.message_handler(commands=['стосунки'])
 def add_friend(message):
@@ -808,6 +760,53 @@ def handle_shower_command(message):
 #     else:
 #         bot.reply_to(message, 'Ця команда доступна лише з 19:00 до 19:30')
 
+def translate_russian_to_ukrainian(word):
+    translation_dict = {
+        'спасиба': 'Дякую',
+        'што': 'що',
+        'шо': 'що',
+        'что': 'що',
+        'чо': 'що',
+        'когда': 'коли',
+        'как': 'як',
+        'где': 'де',
+        'но': 'але',
+        'чиво': 'чого',
+        'чево': 'чого',
+        'чего': 'чого',
+        'да': 'так',
+        'нет': 'ні',
+        'не': 'ні',
+        'канеш': 'звісно',
+        'канешно': 'звісно',
+        'только': 'тільки/лише',
+        'ладно': 'гаразд/окей',
+        'ребят': 'друзі',
+        'даровка': 'здоров',
+        'даров': 'здоров',
+        'хотел': 'здоров',
+        'иди': 'йди/іди',
+        'мать': 'мати',
+        'почему': 'чому',
+        'почти': 'майже',
+        'тебе': 'тобі',
+        'мне': 'мені',
+        'ему': 'йому',
+        # Додайте сюди інші слова та їх переклади
+    }
+    return translation_dict.get(word, word)
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    text = message.text.lower()
+    words = re.findall(r'\b\w+\b', text)  # Знаходимо окремі слова в тексті
+
+    for word in words:
+        ukrainian_word = translate_russian_to_ukrainian(word)
+        if word != ukrainian_word:
+            reply = f"{word} немає в українській мові, правильно {ukrainian_word}"
+            bot.reply_to(message, reply)
+            break
 
 
 @bot.message_handler(func=lambda message: True)
