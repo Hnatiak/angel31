121745007ac6586d6c6834d3f8c25d2bed07ae17
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-03 19:15:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import speakwithbot.communication as communication
-# import translate
+import translate
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -813,6 +813,7 @@ def handle_shower_command(message):
 @bot.message_handler(func=lambda message: True)
 def handle_all_commands(message):
     communication.handle_commands(bot, message)
+    translate.handle_message(bot, message)
     
 # @bot.message_handler(func=lambda message: True)
 # def handle_all_commands(message):
