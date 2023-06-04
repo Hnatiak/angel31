d2aeb85b6abdc519e816097bbcff2208f1edee87
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-04 21:34:31 +0200

    Update translate.py

--- translate.py
+++ translate.py
@@ -329,11 +329,18 @@ def translate_russian_to_ukrainian(word):
     }
     return translation_dict.get(word, word)
 
+# @bot.message_handler(commands=['українські_бали'])
+# def display_scores(bot, message):
+#     reply = "Учасники\n"
+#     for player_id, player in player_scores.items():
+#         player_name = bot.get_chat_member(message.chat.id, player_id).user.first_name
+#         reply += f"{player_name} - {player['score']} {player['quests']} виконаних квестів\n"
+#     bot.reply_to(message, reply)
 @bot.message_handler(commands=['українські_бали'])
 def display_scores(bot, message):
     reply = "Учасники\n"
     for player_id, player in player_scores.items():
-        player_name = bot.get_chat_member(message.chat.id, player_id).user.first_name
+        player_name = message.from_user.first_name if player_id == message.from_user.id else ""
         reply += f"{player_name} - {player['score']} {player['quests']} виконаних квестів\n"
     bot.reply_to(message, reply)
 
