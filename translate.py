55307c3dfeac3181040df1236a3bf22b917eb940
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-04 20:17:11 +0200

    Update translate.py

--- translate.py
+++ translate.py
@@ -327,6 +327,7 @@ def translate_russian_to_ukrainian(word):
     }
     return translation_dict.get(word, word)
 
+
 @bot.message_handler(commands=['українські_бали'])
 def handle_ukrainian_scores(message):
     player_id = message.from_user.id  # Отримуємо ідентифікатор гравця
