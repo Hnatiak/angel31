94336d6930e0169316287cf245525ec11fb6a846
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-04 20:28:46 +0200

    Update translate.py

--- translate.py
+++ translate.py
@@ -186,6 +186,7 @@ bot = telebot.TeleBot(config.TOKEN)
 
 player_scores = {}  # Словник для збереження балів гравців
 QUEST_THRESHOLD = 1000  # Поріг для виконання квесту
+MIN_WORDS_THRESHOLD = 3
 
 def translate_russian_to_ukrainian(word):
     translation_dict = {
