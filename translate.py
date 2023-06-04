656470adb0a820ef01526b3f1f57bedd1bf88908
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-04 20:31:50 +0200

    Update translate.py

--- translate.py
+++ translate.py
@@ -328,6 +328,15 @@ def translate_russian_to_ukrainian(word):
     }
     return translation_dict.get(word, word)
 
+
+@bot.message_handler(commands=['українські_бали'])
+def display_scores(message):
+    reply = "Учасники\n"
+    for player_id, player in player_scores.items():
+        player_name = bot.get_chat_member(message.chat.id, player_id).user.first_name
+        reply += f"{player_name} - {player['score']} {player['quests']} виконаних квестів\n"
+    bot.reply_to(message, reply)
+
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     player_id = message.from_user.id  # Отримуємо ідентифікатор гравця
@@ -363,12 +372,6 @@ def handle_message(message):
             player_scores[player_id]['quests'] += 1
             player_scores[player_id]['score'] = 0
 
-    reply = "Учасники\n"
-    for pid, player in player_scores.items():
-        reply += f"{player_name} - {player['score']} {player['quests']} виконаних квестів\n"
-
-    bot.reply_to(message, reply)
-
 bot.polling()
 
 # @bot.message_handler(func=lambda message: True)
@@ -382,30 +385,31 @@ bot.polling()
 #     text = message.text.lower()
 #     words = re.findall(r'\b\w+\b', text)  # Знаходимо окремі слова в тексті
 
-#     translated_words = []
-#     for word in words:
-#         ukrainian_word = translate_russian_to_ukrainian(word)
-#         if word != ukrainian_word:
-#             translated_words.append((word, ukrainian_word))
+#     if len(words) > MIN_WORDS_THRESHOLD:
+#         translated_words = []
+#         for word in words:
+#             ukrainian_word = translate_russian_to_ukrainian(word)
+#             if word != ukrainian_word:
+#                 translated_words.append((word, ukrainian_word))
 
-#     if translated_words:
-#         reply = ""
-#         for word_pair in translated_words:
-#             reply += f"{word_pair[0]}, "
-#         reply += "немає в українській мові, правильно "
-#         for word_pair in translated_words:
-#             reply += f"{word_pair[1]} "
-#             player_scores[player_id]['score'] -= 1
-#         bot.reply_to(message, reply)
-#     else:
-#         player_scores[player_id]['score'] += 1
+#         if translated_words:
+#             reply = ""
+#             for word_pair in translated_words:
+#                 reply += f"{word_pair[0]}, "
+#             reply += "немає в українській мові, правильно "
+#             for word_pair in translated_words:
+#                 reply += f"{word_pair[1]} "
+#                 player_scores[player_id]['score'] -= 1
+#             bot.reply_to(message, reply)
+#         else:
+#             player_scores[player_id]['score'] += 1
 
-#     # Перевірка виконання квесту
-#     if player_scores[player_id]['score'] >= QUEST_THRESHOLD:
-#         player_scores[player_id]['quests'] += 1
-#         player_scores[player_id]['score'] = 0
+#         # Перевірка виконання квесту
+#         if player_scores[player_id]['score'] >= QUEST_THRESHOLD:
+#             player_scores[player_id]['quests'] += 1
+#             player_scores[player_id]['score'] = 0
 
-#     reply = f"Учасники\n"
+#     reply = "Учасники\n"
 #     for pid, player in player_scores.items():
 #         reply += f"{player_name} - {player['score']} {player['quests']} виконаних квестів\n"
 
