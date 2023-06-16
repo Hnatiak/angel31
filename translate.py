2bb369d8034fce15291dbce2c59423a9d9379d2a
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-16 15:23:11 +0200

    Update translate.py

--- translate.py
+++ translate.py
@@ -375,6 +375,7 @@ def translate_russian_to_ukrainian(word):
     }
     return translation_dict.get(word, word)
 
+@bot.message_handler(func=lambda message: True)
 def handle_message(message):
     player_id = message.from_user.id
     player_name = message.from_user.first_name
