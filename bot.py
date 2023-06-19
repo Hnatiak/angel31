d63ee1c2f9f1bd75d749b67b1b979f7696e20616
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-19 16:51:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -770,23 +770,67 @@ def display_rules(message):
     rules += "4. Після набору 1000 балів гравець отримує +1 виконаний квест, після чого його бали обнуляються автоматично.\n"
     bot.send_message(message.chat.id, rules)
 
+# @bot.message_handler(func=lambda message: True)
+# def handle_message(message):
+#     player_id = message.from_user.id  # Отримуємо ідентифікатор гравця
+#     player_name = message.from_user.first_name  # Отримуємо ім'я гравця
+
+#     if player_id not in player_scores:
+#         player_scores[player_id] = {'score': 0, 'quests': 0}  # Ініціалізуємо бали гравця
+
+#     text = message.text.lower()
+#     words = re.findall(r'\b\w+\b', text)  # Знаходимо окремі слова в тексті
+
+#     if len(words) > MIN_WORDS_THRESHOLD:
+#         translated_words = []
+#         for word in words:
+#             ukrainian_word = translate_russian_to_ukrainian(word)
+#             if word != ukrainian_word:
+#                 translated_words.append((word, ukrainian_word))
+
+#         if translated_words:
+#             reply = ""
+#             for word_pair in translated_words:
+#                 reply += f"{word_pair[0]}, "
+#             reply += "немає в українській мові, правильно "
+#             for word_pair in translated_words:
+#                 reply += f"{word_pair[1]} "
+#                 player_scores[player_id]['score'] -= 1
+#             bot.reply_to(message, reply)
+#         else:
+#             player_scores[player_id]['score'] += 1
+
+#         # Перевірка наявності букв "ё" або "ы" э у слові
+#         for word in words:
+#             if 'ё' in word or 'ы' in word or 'э' in word:
+#                 player_scores[player_id]['score'] -= 1
+
+#         # Перевірка виконання квесту
+#         if player_scores[player_id]['score'] >= QUEST_THRESHOLD:
+#             player_scores[player_id]['quests'] += 1
+#             player_scores[player_id]['score'] = 0
+
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    player_id = message.from_user.id  # Отримуємо ідентифікатор гравця
-    player_name = message.from_user.first_name  # Отримуємо ім'я гравця
+    player_id = message.from_user.id
+    player_name = message.from_user.first_name
 
     if player_id not in player_scores:
-        player_scores[player_id] = {'score': 0, 'quests': 0}  # Ініціалізуємо бали гравця
+        player_scores[player_id] = {'score': 0, 'quests': 0}
 
     text = message.text.lower()
-    words = re.findall(r'\b\w+\b', text)  # Знаходимо окремі слова в тексті
+    words = re.findall(r'\b\w+\b', text)
 
     if len(words) > MIN_WORDS_THRESHOLD:
         translated_words = []
+        has_russian_word = False  # Флаг, що вказує на наявність російського слова
+
         for word in words:
             ukrainian_word = translate_russian_to_ukrainian(word)
             if word != ukrainian_word:
                 translated_words.append((word, ukrainian_word))
+                if is_russian_word(word):
+                    has_russian_word = True
 
         if translated_words:
             reply = ""
@@ -796,16 +840,18 @@ def handle_message(message):
             for word_pair in translated_words:
                 reply += f"{word_pair[1]} "
                 player_scores[player_id]['score'] -= 1
+
+            if has_russian_word:
+                player_scores[player_id]['score'] -= 1
+
             bot.reply_to(message, reply)
         else:
             player_scores[player_id]['score'] += 1
 
-        # Перевірка наявності букв "ё" або "ы" э у слові
         for word in words:
             if 'ё' in word or 'ы' in word or 'э' in word:
                 player_scores[player_id]['score'] -= 1
 
-        # Перевірка виконання квесту
         if player_scores[player_id]['score'] >= QUEST_THRESHOLD:
             player_scores[player_id]['quests'] += 1
             player_scores[player_id]['score'] = 0
