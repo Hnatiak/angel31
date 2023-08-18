56e6265b6b9ab3345a76c5b7e2e03bfb9e856d85
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-08-18 20:46:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,80 +42,80 @@ player_scores = {}  # Словник для збереження балів гр
 QUEST_THRESHOLD = 1000  # Поріг для виконання квесту
 MIN_WORDS_THRESHOLD = 3
 
-# @bot.message_handler(commands=['гра_в_цифри'])
-# def start_number_game(message):
-#     user_id = message.from_user.id
+@bot.message_handler(commands=['гра_в_цифри'])
+def start_number_game(message):
+    user_id = message.from_user.id
 
-#     if user_id in game_numbers:
-#         bot.send_message(chat_id=message.chat.id, text='Ви вже граєте в гру. Спробуйте закінчити попередню гру, прописавши команду /закінчити_гру.')
-#         return
+    if user_id in game_numbers:
+        bot.send_message(chat_id=message.chat.id, text='Ви вже граєте в гру. Спробуйте закінчити попередню гру, прописавши команду /закінчити_гру.')
+        return
 
-#     game_numbers[user_id] = {
-#         'number': random.randint(1, 100),
-#         'attempts_left': None
-#     }
+    game_numbers[user_id] = {
+        'number': random.randint(1, 100),
+        'attempts_left': None
+    }
 
-#     bot.send_message(chat_id=message.chat.id, text='Гра "Вгадай число" розпочата. Вгадайте число від 1 до 100.')
+    bot.send_message(chat_id=message.chat.id, text='Гра "Вгадай число" розпочата. Вгадайте число від 1 до 100.')
 
-# @bot.message_handler(commands=['гра_в_цифри_10', 'гра_в_цифри_9', 'гра_в_цифри_8', 'гра_в_цифри_7', 'гра_в_цифри_6', 'гра_в_цифри_5', 'гра_в_цифри_4', 'гра_в_цифри_3', 'гра_в_цифри_2', 'гра_в_цифри_1'])
-# def start_number_game_with_attempts(message):
-#     user_id = message.from_user.id
+@bot.message_handler(commands=['гра_в_цифри_10', 'гра_в_цифри_9', 'гра_в_цифри_8', 'гра_в_цифри_7', 'гра_в_цифри_6', 'гра_в_цифри_5', 'гра_в_цифри_4', 'гра_в_цифри_3', 'гра_в_цифри_2', 'гра_в_цифри_1'])
+def start_number_game_with_attempts(message):
+    user_id = message.from_user.id
 
-#     if user_id in game_numbers:
-#         bot.send_message(chat_id=message.chat.id, text='Ви вже граєте в гру. Спробуйте закінчити попередню гру, прописавши команду /закінчити_гру.')
-#         return
+    if user_id in game_numbers:
+        bot.send_message(chat_id=message.chat.id, text='Ви вже граєте в гру. Спробуйте закінчити попередню гру, прописавши команду /закінчити_гру.')
+        return
 
-#     attempts_left = int(message.text.split('_')[-1])
-#     if attempts_left < 1 or attempts_left > 10:
-#         bot.send_message(chat_id=message.chat.id, text='Кількість спроб має бути від 1 до 10.')
-#         return
+    attempts_left = int(message.text.split('_')[-1])
+    if attempts_left < 1 or attempts_left > 10:
+        bot.send_message(chat_id=message.chat.id, text='Кількість спроб має бути від 1 до 10.')
+        return
 
-#     game_numbers[user_id] = {
-#         'number': random.randint(1, 100),
-#         'attempts_left': attempts_left
-#     }
+    game_numbers[user_id] = {
+        'number': random.randint(1, 100),
+        'attempts_left': attempts_left
+    }
 
-#     bot.send_message(chat_id=message.chat.id, text=f'Гра "Вгадай число" розпочата. Вгадайте число від 1 до 100. У вас є {attempts_left} спроб.')
+    bot.send_message(chat_id=message.chat.id, text=f'Гра "Вгадай число" розпочата. Вгадайте число від 1 до 100. У вас є {attempts_left} спроб.')
 
-# @bot.message_handler(func=lambda message: message.text.isdigit())
-# def guess_number(message):
-#     user_id = message.from_user.id
+@bot.message_handler(func=lambda message: message.text.isdigit())
+def guess_number(message):
+    user_id = message.from_user.id
 
-#     if user_id not in game_numbers:
-#         bot.send_message(chat_id=message.chat.id, text='Ви ще не почали гру. Почніть гру командою /гра_в_цифри або /гра_в_цифри_(число від 1 - 10 спроб).')
-#         return
+    if user_id not in game_numbers:
+        bot.send_message(chat_id=message.chat.id, text='Ви ще не почали гру. Почніть гру командою /гра_в_цифри або /гра_в_цифри_(число від 1 - 10 спроб).')
+        return
 
-#     game = game_numbers[user_id]
-#     number = game['number']
-#     attempts_left = game['attempts_left']
+    game = game_numbers[user_id]
+    number = game['number']
+    attempts_left = game['attempts_left']
 
-#     guess = int(message.text)
+    guess = int(message.text)
 
-#     if guess == number:
-#         bot.send_message(chat_id=message.chat.id, text='Вітаю! Ви вгадали число!')
-#         del game_numbers[user_id]
-#     elif guess < number:
-#         bot.send_message(chat_id=message.chat.id, text='Загадане число більше.')
-#     else:
-#         bot.send_message(chat_id=message.chat.id, text='Загадане число менше.')
+    if guess == number:
+        bot.send_message(chat_id=message.chat.id, text='Вітаю! Ви вгадали число!')
+        del game_numbers[user_id]
+    elif guess < number:
+        bot.send_message(chat_id=message.chat.id, text='Загадане число більше.')
+    else:
+        bot.send_message(chat_id=message.chat.id, text='Загадане число менше.')
 
-#     if attempts_left is not None:
-#         game['attempts_left'] -= 1
-#         if game['attempts_left'] == 0:
-#             bot.send_message(chat_id=message.chat.id, text=f'Гра закінчена. Ви вичерпали всі спроби. Загадане число було {number}.')
-#             del game_numbers[user_id]
-#         else:
-#             bot.send_message(chat_id=message.chat.id, text=f'У вас залишилося {game["attempts_left"]} спроб.')
+    if attempts_left is not None:
+        game['attempts_left'] -= 1
+        if game['attempts_left'] == 0:
+            bot.send_message(chat_id=message.chat.id, text=f'Гра закінчена. Ви вичерпали всі спроби. Загадане число було {number}.')
+            del game_numbers[user_id]
+        else:
+            bot.send_message(chat_id=message.chat.id, text=f'У вас залишилося {game["attempts_left"]} спроб.')
 
-# @bot.message_handler(commands=['закінчити_гру_в_цифри'])
-# def end_game(message):
-#     user_id = message.from_user.id
+@bot.message_handler(commands=['закінчити_гру_в_цифри'])
+def end_game(message):
+    user_id = message.from_user.id
 
-#     if user_id in game_numbers:
-#         del game_numbers[user_id]
-#         bot.send_message(chat_id=message.chat.id, text='Гра була закінчена.')
-#     else:
-#         bot.send_message(chat_id=message.chat.id, text='Ви не брали участі в жодній грі.')
+    if user_id in game_numbers:
+        del game_numbers[user_id]
+        bot.send_message(chat_id=message.chat.id, text='Гра була закінчена.')
+    else:
+        bot.send_message(chat_id=message.chat.id, text='Ви не брали участі в жодній грі.')
 
 
 
