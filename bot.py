424f19391f1e9c75ff92681ee9f1a5faa5f3ca39
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-11-24 09:01:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -702,25 +702,25 @@ def translate_russian_to_ukrainian(word):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-     player_id = message.from_user.id  # Отримуємо ідентифікатор гравця
+    player_id = message.from_user.id  # Отримуємо ідентифікатор гравця
 
-     text = message.text.lower()
-     words = re.findall(r'\b\w+\b', text)  # Знаходимо окремі слова в тексті
+    text = message.text.lower()
+    words = re.findall(r'\b\w+\b', text)  # Знаходимо окремі слова в тексті
 
-     translated_words = []
-     for word in words:
-         ukrainian_word = translate_russian_to_ukrainian(word)
-         if word != ukrainian_word:
+    translated_words = []
+    for word in words:
+        ukrainian_word = translate_russian_to_ukrainian(word)
+        if word != ukrainian_word:
              translated_words.append((word, ukrainian_word))
 
-     if translated_words:
-         reply = ""
-         for word_pair in translated_words:
-             reply += f"{word_pair[0]}, "
-         reply += "немає в українській мові, правильно "
-         for word_pair in translated_words:
-             reply += f"{word_pair[1]} "
-         bot.reply_to(message, reply)
+    if translated_words:
+        reply = ""
+        for word_pair in translated_words:
+            reply += f"{word_pair[0]}, "
+        reply += "немає в українській мові, правильно "
+        for word_pair in translated_words:
+            reply += f"{word_pair[1]} "
+        bot.reply_to(message, reply)
 
 
 
