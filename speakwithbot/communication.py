c151e2636c4c7f85cf8f94cf7154cc547b76f3e0
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-11-27 22:01:38 +0100

    Update communication.py

--- speakwithbot/communication.py
+++ speakwithbot/communication.py
@@ -17,7 +17,7 @@ random_response_whatimdoing = random.choice(whatimdoing)
 
 
 def get_weather(city):
-    api_key = '3f46be9e408103aa48212de1a723584f'
+    api_key = '0faf4cc80c125af41e3c5cc64ff38cc5'
     base_url = 'http://api.openweathermap.org/data/2.5/weather'
     params = {'q': city, 'appid': api_key, 'units': 'metric'}
     response = requests.get(base_url, params=params)
@@ -34,7 +34,9 @@ def handle_weather_command(message):
     match = re.search(r'\bангел, яка погода в\b(.+)', message.text, re.IGNORECASE)
     if match:
         city = match.group(1).strip()
+        print(f"Місто: {city}")
         weather_response = get_weather(city)
+        print(f"Відповідь про погоду: {weather_response}")
         bot.send_message(message.chat.id, weather_response)
     else:
         bot.send_message(message.chat.id, 'Не вдалося визначити місто у запитанні про погоду.')
