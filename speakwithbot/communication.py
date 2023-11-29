122d859342c82da97dcc6e55682bab0deec355ee
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-11-29 18:42:20 +0100

    Update communication.py

--- speakwithbot/communication.py
+++ speakwithbot/communication.py
@@ -89,8 +89,8 @@ def handle_commands(bot, message):
             
             elif f"{keyword} гімн україни" in text:
                 bot.send_message(message.chat.id, 'Ще не вмерла України і слава, і воля,\nЩе нам, браття молодії, усміхнеться доля.\nЗгинуть наші воріженьки, як роса на сонці,\n\nЗапануєм і ми, браття, у своїй сторонці.\nДушу й тіло ми положим за нашу свободу,\nІ покажем, що ми, браття, козацького роду!')
-            elif f"{keyword} слава україні" in text:
-                bot.send_message(message.chat.id, 'Героям Слава!')
+            elif "ангел слава Україні" in text:
+                bot.send_message(message.chat.id, 'Героям слава!')
             elif f"{keyword} герої не вмирають" in text:
                 bot.send_message(message.chat.id, 'Вмирають Вороги!')
             elif text == f"{keyword} Україна":
