1ee915511d3499c9b8867db600ac6b1f599c5e12
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-07-03 22:53:26 +0200

    Changes

--- bot.py
+++ bot.py
@@ -134,7 +134,7 @@ def handle_commands(message):
         elif re.search(r"\bскільки\b.*\bразів\b.*\bтиждень\b", text_after_keyword):
             bot.send_message(message.chat.id, 'Десь ' + str(random.randint(1, 10)) + ' разів на тиждень')
             answered_question = True
-        elif text_after_keyword.startswith('правда чи дія?') or text_after_keyword.startswith('правда чи дія') in text_after_keyword:
+        elif text_after_keyword in ['правда чи дія?', 'правда чи дія?']:
             bot.send_message(message.chat.id, random.choice(['Правда', 'Дія']))
             answered_question = True
         elif '?' in text_after_keyword:
